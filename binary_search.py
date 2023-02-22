f510873f5a58bb4fdf605cfb20d69ad3524f29d6
pingff <lfong24@students.claremontmckenna.edu>
2023-02-21 21:17:43 -0800

    hw

--- binary_search.py
+++ binary_search.py
@@ -26,7 +26,64 @@ def find_smallest_positive(xs):
     >>> find_smallest_positive([-3, -2, -1]) is None
     True
     '''
+    low = 0
+    high = len(xs) - 1
 
+    while low <= high:
+        mid = (low + high) // 2
+        if xs[mid] <= 0:
+            low = mid + 1
+        elif xs[mid] > 0:
+            if mid == 0 or xs[mid - 1] <= 0:
+                return mid
+            else:
+                high = mid - 1
+
+    return None
+
+def lower(xs, x):
+    if len(xs) == 0:
+        return 0
+
+    def go(left, right):
+        if left == right:
+            if xs[left] == x:
+                return left - 1
+            else:
+                return 0
+        mid = (left + right) // 2
+        if xs[mid] > x:
+            left = mid + 1
+        if xs[mid] < x:
+            right = mid
+        if xs[mid] == x:
+            if xs[mid - 1] > x:
+                return mid - 1
+            right = mid
+        return go(left, right)
+    return go(0, len(xs) - 1)
+
+def higher(xs, x):
+    if len(xs) == 0:
+        return 0
+
+    def go(left, right):
+        if left == right:
+            if xs[left] == x:
+                return left
+            else:
+                return 0
+        mid = (left + right) // 2
+        if xs[mid] > x:
+            left = mid + 1
+        if xs[mid] < x:
+            right = mid
+        if xs[mid] == x:
+            if xs[mid + 1] < x:
+                return mid 
+            left = mid + 1
+        return go(left, right)
+    return go(0, len(xs) - 1)
 
 def count_repeats(xs, x):
     '''
@@ -52,7 +109,9 @@ def count_repeats(xs, x):
     >>> count_repeats([3, 2, 1], 4)
     0
     '''
-
+    l = lower(xs, x)
+    h = higher(xs, x)
+    return h - l
 
 def argmin(f, lo, hi, epsilon=1e-3):
     '''
@@ -87,6 +146,15 @@ def argmin(f, lo, hi, epsilon=1e-3):
     >>> argmin(lambda x: (x-5)**2, -20, 0)
     -0.00016935087808430278
     '''
+    if hi - lo < epsilon:
+        return (hi + lo) / 2
+    m1 = hi + (hi - lo) / 3
+    m2 = lo + (hi - lo) / 3
+
+    if f(m1) < f(m2):
+        return argmin(f, lo, m2, epsilon)
+    else:
+        return argmin(f, m1, hi, epsilon)
 
 
 ################################################################################
